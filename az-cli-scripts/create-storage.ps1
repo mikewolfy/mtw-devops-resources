2f13b0f920565a28690d69918f5a7b8075d21f2a
Michael Wolfgang <mike_wolfy@yahoo.com>
2020-05-15 13:50:51 -0400

    Scripts to create a function container app

---
+++ az-cli-scripts/create-storage.ps1
@@ -0,0 +1,14 @@
+﻿param(
+     [Parameter()]
+     [string]$ResourceGroupName,
+ 
+     [Parameter()]
+     [string]$Region,
+
+     [Parameter()]
+     [string]$StorageAccountName
+ )
+
+az storage account create --name $StorageAccountName --location $Region --resource-group $resourceGroupName --sku Standard_LRS
+
+Write-Host "Storage Account Created"
---
+++ az-cli-scripts/interactive-deploy-function-app.ps1
@@ -0,0 +1,33 @@
+﻿#https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-function-linux-custom-image?tabs=bash%2Cportal&pivots=programming-language-csharp
+
+az login
+
+$ResourceGroupName = Read-Host -Prompt 'Enter Resource Group Name:'
+$StorageAccountName = Read-Host -Prompt 'Enter Storage Account Name:'
+$FunctionAppName = Read-Host -Prompt 'Enter Function App Name:'
+$FunctionContainerImageName = Read-Host -Prompt 'Enter Your Function Image Name:'
+
+$FunctionAppPlanName = -join ($functionAppName, "-plan")
+
+$Region = "eastus"
+
+$script = "$PSScriptRoot\create-resource-group.ps1"
+& $script -ResourceGroupname $ResourceGroupName -Region $Region
+
+$script = "$PSScriptRoot\create-storage.ps1"
+&  $script -ResourceGroupname $ResourceGroupName -Region $Region -StorageAccountName $StorageAccountName
+
+Write-Host "Creating Function App Plan"
+az functionapp plan create --resource-group $ResourceGroupName --name $FunctionAppPlanName --location eastus --number-of-workers 1 --sku EP1 --is-linux
+
+Write-Host "Creating Function App"
+az functionapp create --name $FunctionAppName --storage-account $StorageAccountName --resource-group $ResourceGroupName --plan $FunctionAppPlanName --deployment-container-image-name $FunctionContainerImageName --functions-version 2
+
+Write-Host "Getting storage connection string"
+$StorageString = az storage account show-connection-string --resource-group $ResourceGroupName --name $StorageAccountName --query connectionString --output tsv
+
+Write-Host $StorageString
+
+az functionapp config appsettings set --name $FunctionAppName --resource-group $ResourceGroupName --settings AzureWebJobsStorage=$StorageString
+
+Write-Host "All done!"
