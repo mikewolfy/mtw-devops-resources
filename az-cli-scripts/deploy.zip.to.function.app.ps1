85da00c026a0140908a85eb1000a5d24e25d603f
Michael Wolfgang <mike_wolfy@yahoo.com>
2020-08-20 08:21:22 -0400

    Script updates

--- az-cli-scripts/deploy.zip.to.function.app.ps1
+++ az-cli-scripts/deploy.zip.to.function.app.ps1
@@ -1,7 +1,12 @@
-## FIRST: run 'dotnet publish' from the root of your Functions Project directory, 
-## SECOND: zip up the output folder, which should have host.json in the root
-## THIRD: run this command to deploy to your function app
+# https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push
+
+# FIRST: run 'dotnet publish' from the root of your Functions Project directory, 
+# SECOND: zip up the output folder, which should have host.json in the root
+# THIRD: run this command to deploy to your function app
 
 az login
 
-az functionapp deployment source config-zip -g "func222-rg" -n "func-name-987" --src "<<Path to Zip>>"
+#az functionapp deployment source config-zip -g "kmx-qa-central-shops-ciam-shared" -n "mycarmax-id-proof-function-svc-qa-central" --src "C:\Users\149036\Downloads\app.zip"
+
+az functionapp deployment source config-zip -g "kmx-dev-east-shops-ciam" -n "mtw-test-func22" --src "C:\Users\149036\Downloads\app.zip"
+
---
+++ az-cli-scripts/query.deployments.ps1
@@ -0,0 +1,16 @@
+﻿az login
+az account set --subscription "<Subscription Name>"
+$deployments = az deployment group list --resource-group "<group name>"
+
+$first = true;
+
+foreach($deployment in $deployments)
+{
+  Write-Host $deployment.Name + ", " + $deployment.ProvisioningState
+
+  if ($first)
+  {
+    Write-Host $deployment
+    first = false
+  }
+}
